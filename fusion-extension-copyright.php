68a5fc105b4e57ca508d2638cc91b899d37b54d6
Jeff Chew <jeff.chew@agencydominion.com>
2016-09-26 14:05:52 -0300

    Update plugin URL

--- fusion-extension-copyright.php
+++ fusion-extension-copyright.php
@@ -4,7 +4,7 @@
  */
 /**
  * Plugin Name: Fusion : Extension - Copyright
- * Plugin URI: http://fusion.1867dev.com
+ * Plugin URI: http://www.agencydominion.com/fusion/
  * Description: Copyright Extension Package for Fusion.
  * Version: 1.1.0
  * Author: Agency Dominion
